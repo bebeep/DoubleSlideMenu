6a10f2e1a8b0b1b1c5e25b5db533cc6760e8dc01
bebeep <424468648@qq.com>
2018-11-16 10:19:05 +0800

    add screenshots

--- app/src/main/java/com/bebeep/doubleslidemenu/MainActivity.java
+++ app/src/main/java/com/bebeep/doubleslidemenu/MainActivity.java
@@ -1,13 +1,77 @@
 package com.bebeep.doubleslidemenu;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import com.bebeep.doubleslidemenu.databinding.ActivityMainBinding;
+import com.bebeep.slidemenu.DoubleSlideMenu;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        init();
     }
+
+    @SuppressLint("NewApi")
+    private void init(){
+
+        binding.flMain.setMySlideMenu(binding.msmParent);
+        binding.msmParent.setMenuLocation(DoubleSlideMenu.DragState.LEFT);
+        binding.msmParent.setOffsetX(0.6f);
+        binding.msmParent.setOnDragstateChangeListener(new DoubleSlideMenu.onDragStateChangeListener() {
+            @Override
+            public void onOpen() {
+                Log.e("TAG","open");
+            }
+
+            @Override
+            public void onClose() {
+                Log.e("TAG","close");
+            }
+
+            @Override
+            public void onDraging(float fraction) {
+//                Log.e("TAG","Draging:"+fraction);
+            }
+        });
+
+
+        binding.tvMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("TAG","text");
+            }
+        });
+
+    }
+
+
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_open:
+                binding.msmParent.setMenuLocation(DoubleSlideMenu.DragState.LEFT);
+                break;
+            case R.id.btn_close:
+                binding.msmParent.setMenuLocation(DoubleSlideMenu.DragState.RIGHT);
+                break;
+        }
+    }
+
+
+
+
+
 }
---
+++ slidemenu/src/main/java/com/bebeep/slidemenu/MainLayout.java
@@ -0,0 +1,51 @@
+package com.bebeep.slidemenu;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.FrameLayout;
+
+public class MainLayout extends FrameLayout {
+
+    private DoubleSlideMenu mySlideMenu;
+
+    public MainLayout(@NonNull Context context) {
+        super(context);
+    }
+
+
+    public MainLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public MainLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    public void setMySlideMenu(DoubleSlideMenu mySlideMenu){
+        this.mySlideMenu = mySlideMenu;
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if(mySlideMenu !=null && mySlideMenu.getDragState() == DoubleSlideMenu.DragState.STATE_OPEN){
+            //如果该侧滑面板是打开，则拦截消费触摸事件
+            return true;
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(mySlideMenu !=null && mySlideMenu.getDragState() == DoubleSlideMenu.DragState.STATE_OPEN){
+            if(event.getAction() == MotionEvent.ACTION_UP) mySlideMenu.close();//如果该侧滑面板是打开，则拦截消费触摸事件
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+}
